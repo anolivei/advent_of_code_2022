c023a60ee04b13c6a210a3f5e5d1040161d1a8a7
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2022-12-03 23:17:38 -0300

    day2 2

--- day02_Rock_Paper_Scissors/main.go
+++ day02_Rock_Paper_Scissors/main.go
@@ -6,36 +6,81 @@ import (
 	"strings"
 )
 
+const (
+	A = "A"
+	B = "B"
+	C = "C"
+	X = "X"
+	Y = "Y"
+	Z = "Z"
+)
+
 func won(ret []string) int {
-	if (ret[0] == "A" && ret[1] == "Y") ||
-		(ret[0] == "B" && ret[1] == "Z") ||
-		(ret[0] == "C" && ret[1] == "X") {
+	if (ret[0] == A && ret[1] == Y) ||
+		(ret[0] == B && ret[1] == Z) ||
+		(ret[0] == C && ret[1] == X) {
 		return 6
 	}
 	return 0
 }
 
 func draw(ret []string) int {
-	if (ret[0] == "A" && ret[1] == "X") ||
-		(ret[0] == "B" && ret[1] == "Y") ||
-		(ret[0] == "C" && ret[1] == "Z") {
+	if (ret[0] == A && ret[1] == X) ||
+		(ret[0] == B && ret[1] == Y) ||
+		(ret[0] == C && ret[1] == Z) {
 		return 3
 	}
 	return 0
 }
 
 func shapeSelected(ret []string) int {
-	if ret[1] == "X" {
+	if ret[1] == X {
 		return 1
 	}
-	if ret[1] == "Y" {
+	if ret[1] == Y {
 		return 2
 	}
 	return 3
 }
 
+func isRock(ret []string) int {
+	if ret[0] == A && ret[1] == Y ||
+		ret[0] == B && ret[1] == X ||
+		ret[0] == C && ret[1] == Z {
+			return 1
+		}
+	return 0
+}
+
+func isPaper(ret []string) int {
+	if ret[0] == A && ret[1] == Z ||
+		ret[0] == B && ret[1] == Y ||
+		ret[0] == C && ret[1] == X {
+			return 2
+		}
+	return 0
+}
+
+func isScissors(ret []string) int {
+	if ret[0] == A && ret[1] == X ||
+		ret[0] == B && ret[1] == Z ||
+		ret[0] == C && ret[1] == Y {
+			return 3
+		}
+	return 0
+}
+
+func wonOrDraw(ret []string) int {
+	if ret[1] == Y {
+		return 3
+	}
+	if ret[1] == Z {
+		return 6
+	}
+	return 0
+}
+
 func main() {
-	var ret [][]string
 	var ret1 int
 	var ret2 int
 	dat, err := os.ReadFile("./input.txt")
@@ -46,7 +91,7 @@ func main() {
 	for i := 0; i < len(datString) - 1; i++ {
 		dup := strings.Split(string(datString[i]), " ")
 		ret1 += shapeSelected(dup) + draw(dup) + won(dup)
-		ret = append(ret, dup)
+		ret2 += wonOrDraw(dup) + isRock(dup) + isPaper(dup) + isScissors(dup)
 	}
 	fmt.Println("--- Part One --- ", ret1)
 	fmt.Println("--- Part Two --- ", ret2)
