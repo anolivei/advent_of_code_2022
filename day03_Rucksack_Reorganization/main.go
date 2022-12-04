1bb8ae1807a744eb56a125addb1d7067b80b9b84
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2022-12-04 01:57:00 -0300

    day3 1

--- day03_Rucksack_Reorganization/main.go
+++ day03_Rucksack_Reorganization/main.go
@@ -22,10 +22,22 @@ func decode(s string) rune {
 	return num
 }
 
+func partOne(s string) int {
+	var letter string
+	for i := 0; i < len(s) / 2; i++ {
+		for j := len(s) / 2; j < len(s); j++ {
+			if s[i] == s[j] {
+				letter = string(s[i])
+				break
+			}
+		}
+	}
+	return int(decode(letter))
+}
+
 func main() {
 	var ret1 int
 	var ret2 int
-	var letter string
 
 	dat, err := os.ReadFile("./input.txt")
 	if err != nil {
@@ -33,14 +45,7 @@ func main() {
 	}
 	datString := strings.Split(string(dat), "\n")
 	for i := 0; i < len(datString) - 1; i++ {
-		for j := 0; j < len(datString[i]) / 2; j++ {
-			for k := len(datString[i]) / 2; k < len(datString[i]); k++ {
-				if datString[i][j] == datString[i][k] {
-					letter = string(datString[i][j])
-					break
-				}
-		}}
-		ret1 += int(decode(letter))
+		ret1 += partOne(datString[i])
 	}
 	fmt.Println("--- Part One --- ", ret1)
 	fmt.Println("--- Part Two --- ", ret2)
